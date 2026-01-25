d0a319a45f14529573d16db106304322f642b038
KirilStefanovski226037 <kstef3849@gmail.com>
2026-01-25 19:43:46 +0100

    Structure refactoring, problem class creation and problem resource examples added

---
+++ scripts/problems/EmailProblem.gd
@@ -0,0 +1,8 @@
+﻿extends Problem
+class_name EmailProblem
+
+@export var sender: String
+@export var subject: String
+@export var body_preview: String
+@export var has_attachment: bool = false
+@export var suspicious_flags: Array[String] = []
